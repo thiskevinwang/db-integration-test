1ff5ef32ec37ed662f4367496125f40845cf5d61
Kevin Wang <kwangsan@gmail.com>
2022-04-30 00:25:50 -0400

    chore: cleanup pool

--- db/pool.ts
+++ db/pool.ts
@@ -7,15 +7,8 @@ const config: PoolConfig = {
   database: "postgres",
   user: "postgres",
   password: process.env.POSTGRES_PASSWORD,
-  port: 5432,
+  port: Number(process.env.POSTGRES_PORT),
   host: process.env.POSTGRES_HOST,
-  idleTimeoutMillis: 120_000,
-  // lambda timeout is 30
-  connectionTimeoutMillis: 16_000,
-  ssl:
-    process.env.NODE === "production"
-      ? { rejectUnauthorized: false }
-      : undefined,
 };
 
 export const pool = new Pool(config);
